76cf8d98bb87eb62095fa8ebe46b3c8e245c2e2a
John <johnaoraha@gmail.com>
2018-06-15 11:39:43 -0400

    created homecontroller

--- src/main/java/com/cryptocurrency/CryptoCrazy/HomeController.java
+++ src/main/java/com/cryptocurrency/CryptoCrazy/HomeController.java
@@ -1,8 +1,17 @@
 package com.cryptocurrency.CryptoCrazy;
 
+import java.util.Date;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cryptocurrency.CryptoCrazy.model.Crypto;
+
 
 @Controller
 public class HomeController {
@@ -11,8 +20,18 @@ public class HomeController {
 	public ModelAndView index() {
 		return new ModelAndView ("index");
 	}
-
-
-
+	
+	@RequestMapping("pricechange")
+	public ModelAndView Price(@RequestParam("crypto") String crypto, @RequestParam("dateStart") Date dateStart, @RequestParam("dateEnd") Date dateEnd) {
+	
+		RestTemplate restTemplate = new RestTemplate();
+		Crypto price1 = restTemplate.getForObject("https://min-api.cryptocompare.com/data/pricehistorical?fsym=BCH&tsyms=USD&ts=1525910400", Crypto.class);
+//		Crypto price2 = restTemplate.getForObject("https://min-api.cryptocompare.com/data/pricehistorical?fsym=BCH&tsyms=USD&ts=1527638400", Crypto.class);
+//		double pricechange = price1 - price2;
+		
+		System.out.println(price1);
+		return new ModelAndView("index", "crypto", price1); // page, expressions language tag reference, object
+																		// you want to send back in
+	}
+	
 }
-
