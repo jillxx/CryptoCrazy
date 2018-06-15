7c255393fc256fc91c97b316e158744ef791878d
John <johnaoraha@gmail.com>
2018-06-15 16:01:38 -0400

    created pojos for individual cryptos

---
+++ src/main/java/com/cryptocurrency/CryptoCrazy/model/VEN.java
@@ -0,0 +1,30 @@
+package com.cryptocurrency.CryptoCrazy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VEN {
+	
+	@JsonProperty("USD")
+	private double USD;
+
+	public VEN() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getUSD() {
+		return USD;
+	}
+
+	public void setUSD(double uSD) {
+		USD = uSD;
+	}
+
+	@Override
+	public String toString() {
+		return "VEN [USD=" + USD + "]";
+	}
+	
+	
+}
