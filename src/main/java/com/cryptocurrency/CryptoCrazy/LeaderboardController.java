4ffed5cb63e3a2b93abba7757ac7839081a72702
MikeDetroit <mikedetroit@gmail.com>
2018-06-18 13:46:48 -0400

    leaderboard changes

--- src/main/java/com/cryptocurrency/CryptoCrazy/LeaderboardController.java
+++ src/main/java/com/cryptocurrency/CryptoCrazy/LeaderboardController.java
@@ -1,19 +1,31 @@
 package com.cryptocurrency.CryptoCrazy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cryptocurrency.CryptoCrazy.dao.LeaderboardRepo;
+import com.cryptocurrency.CryptoCrazy.model.Leaderboard;
 
 
 
+@Controller
 public class LeaderboardController {
 	
 	@Autowired
+	LeaderboardRepo lr;
 	
-	@RequestMapping("/") // can get either getmapping or requestmapping not postmapping here tho
+	
+	@RequestMapping("/leaderboard") // can get either getmapping or requestmapping not postmapping here tho
 	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView("index"); // page of the jsp that should be returned
+		List<Leaderboard> leaderBoard = new ArrayList<>();
+		leaderBoard = lr.findAll();
+		System.out.println(leaderBoard);
+		ModelAndView mv = new ModelAndView("leaderboard"); // page of the jsp that should be returned
 
 		return mv;
 	
